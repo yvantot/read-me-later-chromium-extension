8d0798738439716f22eeab348d307e722bdbf7bf
Jun Ivanne Dalman <123827898+yvantot@users.noreply.github.com>
2024-07-01 21:45:24 +0800

    Update content.js

--- contentscript/content.js
+++ contentscript/content.js
@@ -1,2 +1,3 @@
 // A very confusing scenario but this extension doesn't work properly without a content script.
+// Yes, even if it's empty. I tried everything; modifying the manifest file, removing this file, hunting dependencies on this file-- but none
 // The issue is the favicon doesn't load properly, and Chromium browsers are throwing an error, stating that I need permissions.
