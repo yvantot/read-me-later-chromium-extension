a42d0c531d96eb5fb4b3696dbc122ce9cb5301d0
Jun Ivanne Dalman <123827898+yvantot@users.noreply.github.com>
2024-07-01 21:41:03 +0800

    Delete readlater directory

--- readlater/contentscript/content.js
+++
@@ -1,8 +0,0 @@
-/* const header = document.createElement('header');
-const shadowRoot = header.attachShadow({ mode: 'open' });
-shadowRoot.innerHTML = '<h1>SHADOW DOM BABY!</h1>';
-
-document.body.appendChild(header);
-
-alert('Running!');
- */
