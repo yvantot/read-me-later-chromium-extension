4a78f55f2b18f13d384da692d961b9af9e4e1568
Jun Ivanne Dalman <123827898+yvantot@users.noreply.github.com>
2024-07-01 21:07:06 +0800

    First commit

---
+++ contentscript/content.js
@@ -0,0 +1,8 @@
+/* const header = document.createElement('header');
+const shadowRoot = header.attachShadow({ mode: 'open' });
+shadowRoot.innerHTML = '<h1>SHADOW DOM BABY!</h1>';
+
+document.body.appendChild(header);
+
+alert('Running!');
+ */
